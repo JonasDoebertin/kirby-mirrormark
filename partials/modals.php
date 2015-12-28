7ef84d11a4c08654d1bba01f6daab25a7ac5d0bc
Thomas Günther <mail@medienbaecker.com>
2015-12-28 15:38:13 +0100

    Multiple Instances Fix

--- partials/modals.php
+++ partials/modals.php
@@ -108,19 +108,21 @@
           <div class="rootpages">
             <? 
             $subpages = site()->children();
-            function listpages($subpages) {
-              foreach ($subpages as $subpage) {
-                echo('<div class="page"><div class="pagename">');
-                echo('<span class="name">' . $subpage->title() . '</span>');
-                if ($subpage->num() != "") $number = $subpage->num();
-                else $number = "–";
-                echo('<span class="number smallbox">' . $number . '</span>');
-                echo('<span class="link smallbox active smalllink" data-link="' . $subpage->uri() . '"><i class="icon fa fa-link"></i></span>');
-                if($subpage->children()->count() > 0) echo('<span class="slidedown active smallbox smalllink"><i class="icon fa fa-angle-down"></i></span>');
-                else echo('<span class="slidedown smallbox smalllink">–</span>');
-                echo('</div><div class="subpages d' . $subpage->depth() . '" style="background-color: rgba(0,0,0,' . $subpage->depth()/25 . ')">');
-                if($subpage->children()->count() > 0)listpages($subpages = $subpage->children());
-                echo('</div></div>');
+            if (!function_exists("listpages")) {
+              function listpages($subpages) {
+                foreach ($subpages as $subpage) {
+                  echo('<div class="page"><div class="pagename">');
+                  echo('<span class="name">' . $subpage->title() . '</span>');
+                  if ($subpage->num() != "") $number = $subpage->num();
+                  else $number = "–";
+                  echo('<span class="number smallbox">' . $number . '</span>');
+                  echo('<span class="link smallbox active smalllink" data-link="' . $subpage->uri() . '"><i class="icon fa fa-link"></i></span>');
+                  if($subpage->children()->count() > 0) echo('<span class="slidedown active smallbox smalllink"><i class="icon fa fa-angle-down"></i></span>');
+                  else echo('<span class="slidedown smallbox smalllink">–</span>');
+                  echo('</div><div class="subpages d' . $subpage->depth() . '" style="background-color: rgba(0,0,0,' . $subpage->depth()/25 . ')">');
+                  if($subpage->children()->count() > 0)listpages($subpages = $subpage->children());
+                  echo('</div></div>');
+                }
               }
             }
             listpages($subpages = site()->children());
